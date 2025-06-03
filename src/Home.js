e3b08d4f63af51abc1f0f4afea0a65089e8e0b33
Jeeva <144939263+Jeevak07@users.noreply.github.com>
2025-06-03 18:01:17 +0530

    chore: set Node 22 for Netlify

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+
 import './policyCard.css'
 
 function Home() {
-  // const location = useLocation();
-  // const user = location.state?.user;
   const [policies,setPolicies] = useState([]);
 
   useEffect(() => {
